a55ec1681aa31c33465807a4c0f1e91a50aa5fc6
SebassBarreto <luscorreab@udistrital.edu.co>
2025-12-04 19:50:02 -0500

    Modelo Region.h

---
+++ src/modelos/Region.h
@@ -0,0 +1,46 @@
+#ifndef REGION_H
+#define REGION_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Region{
+private:
+    int id;
+    string nombre;
+    string descripcion;
+public:
+    Region(): id(0), nombre(""), descripcion(""){}
+    Region(int i, const string& n, const string& d): id(i), nombre(n), descripcion(d){}
+    
+    //getters
+    int getId() const{
+        return id;
+    }
+    const string& getNombre() const{
+        return nombre;
+    }
+
+    const string& getDescripcion() const{
+        return descripcion;
+    }
+
+    //setters
+    void setId(int i){
+        id = i;
+    }
+
+    void setNombre(const string& n){
+        nombre = n;
+    }
+
+    void setDescripcion(const string& d){
+        descripcion = d;
+    }
+};
+
+
+
+#endif //REGION_H
